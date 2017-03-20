8453ef116b24bded90c9e133016d271bdb4de0d7
skovic <skovic@lenovo.com>
2017-03-20 17:49:39 -0400

    Add firmware details to server summary page

--- app/helpers/physical_server_helper/textual_summary.rb
+++ app/helpers/physical_server_helper/textual_summary.rb
@@ -10,6 +10,10 @@ module PhysicalServerHelper::TextualSummary
   def textual_group_compliance
   end
 
+  def textual_group_firmware_details
+    %i(firmware_details)
+  end
+
   def textual_host
     {:label => _("Host"), :value => @record.host&.name, :icon =>  "pficon pficon-virtual-machine", :link => url_for(:controller =>'host', :action =>  'show', :id =>  @record.host&.id)}
   end
@@ -36,5 +40,14 @@ module PhysicalServerHelper::TextualSummary
 
    def textual_model
       {:label =>  _("Model"), :value  =>  @record.model}
+   end
+
+   def textual_firmware_details
+     fw_details = []
+     @record.firmware.each do |fw|
+       fw_details.push({:label => fw.name, :value => fw.version})
+     end
+
+     {:value => fw_details}
    end
 end
